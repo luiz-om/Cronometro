d307647c11b3219ff98da7b4e2fb5be2c50c74d1
Luiz-OM <luizfernando_martins@hotmail.com>
2023-02-07 10:48:42 -0400

    Finalização do Cronometro.

--- Program.cs
+++ Program.cs
@@ -1,31 +1,56 @@
-﻿static void Menu(){
+﻿
+Menu();
+
+
+static void Menu()
+{
     Console.Clear();
     Console.WriteLine("S = Segundo");
     Console.WriteLine("M = Minuto");
     Console.WriteLine("0 = sair");
     Console.WriteLine("Quanto tempo deseja contar?");
 
-   string data = Console.ReadLine().ToLower();
-   char type =char.Parse(data.Substring(data.Length-1, 1));
- int time = int.Parse(data.Substring(0, data.Length-1));
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length - 1, 1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+    int multiplier = 1;
+
+    if (type == 'm')
+        multiplier = 60;
+
+    if (time == 0)
+        System.Environment.Exit(0);
+
+
+    PreStart(time * multiplier);
 
 }
-static void Start(int time){
-    int currentTime=0;
 
-while (currentTime != time)
+static void PreStart(int time)
 {
     Console.Clear();
-    currentTime++;
-    System.Console.WriteLine(currentTime);
+    System.Console.WriteLine("Ready...");
     Thread.Sleep(1000);
+    System.Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    System.Console.WriteLine("Go...");
+    Thread.Sleep(2500);
+
+    Start(time);
 }
-Console.Clear();
-System.Console.WriteLine("StropWAtch Finalizado.");
-Thread.Sleep(2500);
+
+static void Start(int time)
+{
+    int currentTime = time;
+
+    while (currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        System.Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+    Console.Clear();
+    System.Console.WriteLine("StropWAtch Finalizado.");
+    Thread.Sleep(2500);
 }
-
-
-Menu();
-
-//Start(10);
