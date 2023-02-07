957d57f3fb432adaa115227685e0b80b7407fbcc
Luiz-OM <luizfernando_martins@hotmail.com>
2023-02-07 10:10:36 -0400

    Pegando primeiro e ultimo caractere da String

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
-﻿int time =10;
-int currentTime=0;
+﻿static void Menu(){
+    Console.Clear();
+    Console.WriteLine("S = Segundo");
+    Console.WriteLine("M = Minuto");
+    Console.WriteLine("0 = sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+   string data = Console.ReadLine().ToLower();
+   char type =char.Parse(data.Substring(data.Length-1, 1));
+ int time = int.Parse(data.Substring(0, data.Length-1));
+
+}
+static void Start(int time){
+    int currentTime=0;
 
 while (currentTime != time)
 {
@@ -8,3 +20,12 @@ while (currentTime != time)
     System.Console.WriteLine(currentTime);
     Thread.Sleep(1000);
 }
+Console.Clear();
+System.Console.WriteLine("StropWAtch Finalizado.");
+Thread.Sleep(2500);
+}
+
+
+Menu();
+
+//Start(10);
